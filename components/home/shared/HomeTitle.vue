68b75e9fd738a39425a09834c5c4f784d5fd809c
aaelrahman89 <45231609+aaelrahman89@users.noreply.github.com>
2020-09-13 12:16:01 +0200

    ..

--- components/home/shared/HomeTitle.vue
+++ components/home/shared/HomeTitle.vue
@@ -1,35 +1,35 @@
 <template>
-    <div class="text-center">
-        <h5 class="m-0 position-relative">{{ sig }}</h5>
-        <h3 class="text-black m-0 position-relative">{{ title }}</h3>
-        <p class="text-black m-0">{{ desc }}</p>
-    </div>
+  <div class="text-center">
+    <h5 class="m-0 position-relative">{{ sig }}</h5>
+    <h3 class="text-black m-0 position-relative">{{ title }}</h3>
+    <p class="text-black m-0">{{ desc }}</p>
+  </div>
 </template>
 
 <script>
 export default {
-    props: {
-      title: {
+  props: {
+    title: {
       type: String,
-      required: true
+      required: true,
     },
     desc: {
-      type: String
+      type: String,
     },
     sig: {
       type: String,
-      default: 'Andalusia For Ladies Wellness'
-    }
-    }
-}
+      default: "Andalusia For Ladies Wellness",
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-div{
-  margin: 92px 0px 135px 0px;
+div {
+  margin: 92px 0px 130px 0px;
 }
-    h5{
-      font-family: 'Conv_TuesdayNight-Regular',Sans-Serif;
+h5 {
+  font-family: "Conv_TuesdayNight-Regular", Sans-Serif;
   opacity: 0.8;
   font-size: 50px;
   font-weight: normal;
@@ -37,22 +37,22 @@ div{
   font-style: normal;
   letter-spacing: normal;
   color: #d18c2f;
-    top: 50px;
-    z-index: 1;
-    }
-    h3{
-font-size: 100px;
+  top: 50px;
+  z-index: 1;
+}
+h3 {
+  font-size: 100px;
   font-weight: 300;
   font-stretch: normal;
   font-style: normal;
   letter-spacing: normal;
-    z-index: 2;
-    }
-    p{
+  z-index: 2;
+}
+p {
   font-size: 30px;
   font-weight: 300;
   font-stretch: normal;
   font-style: normal;
   letter-spacing: normal;
-    }
+}
 </style>
